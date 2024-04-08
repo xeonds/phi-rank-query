8e8ca43320f2a5af4780c4b1c885ba71aa2b4218
xeonds <xeonds@stu.xidian.edu.cn>
2024-04-08 09:39:32 +0800

    feat: add more pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,16 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const pathSrc = path.resolve(__dirname, 'src')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': pathSrc,
+        },
+    },
   server: {
     proxy: {
       "/api/v1": {
