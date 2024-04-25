a22eca28c7862cd86dce9dc55f3ce9d57c0829de
xeonds <xeonds@stu.xidian.edu.cn>
2024-04-26 02:58:42 +0800

    feat: calc 2/3

--- web/src/views/calc.vue
+++ web/src/views/calc.vue
@@ -9,50 +9,60 @@
         </div>
         <div v-if="!editVisible" class="flex flex-row justify-around">
             <div class="flex flex-col px-8">
-                <template v-for="(_, index) in rankTable">
+                <SongItem v-if="true" :index="`#phi`" :song="b0" />
+                <template v-for="(_, index) in b19">
                     <SongItem v-if="index % 2 == 0" :index="`#${index}`" :song="rankTable[index]" />
                 </template>
             </div>
             <div class="flex flex-col px-8">
-                <template v-for="(_, index) in rankTable">
+                <template v-for="(_, index) in b19">
                     <SongItem v-if="index % 2 == 1" :index="`#${index}`" :song="rankTable[index]" />
                 </template>
             </div>
         </div>
-        <div v-else class="flex items-center mb-4">
-            <input type="text" v-model="curr" placeholder="选择id"
-                class="input input-ghost input-bordered w-full max-w-xs mr-2 p-2 border border-gray-800 rounded" />
-            <input type="text" v-model="rankTable[curr].difficulty" placeholder="输入歌曲定数"
-                class="input input-ghost input-bordered w-full max-w-xs mr-2 p-2 border border-gray-800 rounded" />
-            <input type="text" v-model="rankTable[curr].acc" placeholder="输入歌曲准度"
-                class="input input-ghost input-bordered w-full max-w-xs mr-2 p-2 border border-gray-800 rounded" />
+        <div v-else class="flex items-center mb-4 *:mx-2">
+            <button @click="curr < 19 ? curr++ : curr" class="btn btn-outline btn-primary"> + </button>
+            <button @click="curr > 0 ? curr-- : curr" class="btn btn-outline btn-primary"> - </button>
+            <span class="mx-4">{{ curr }}</span>
+            <select class="select select-ghost select-bordered w-full max-w-xs" v-model="b19[curr].song"
+                placeholder="选择歌曲id">
+                <option v-for="(item, index) in rankTable" :value="index">{{ item.title }}</option>
+            </select>
+            <select class="select select-ghost select-bordered w-full max-w-xs" v-model="b19[curr].difficulty"
+                placeholder="选择难度">
+                <option v-for="item in songDifficulties" :value="item">{{ item }}</option>
+            </select>
+            <input type="text" v-model="b19[curr].acc" placeholder="输入准度" class="input input-ghost input-bordered" />
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import SongItem from '@/components/song.vue';
-import { Ref, onMounted, ref } from 'vue';
+import { Ref, onMounted, ref, watch } from 'vue';
+import { Song } from '@/common';
 
-interface RankTableItem {
-    num: number;
-    song: string;
-    illustration: string;
-    rank: string;
-    difficulty: string;
-    rks: string;
-    Rating: string;
-    score: string;
-    acc: string;
-    suggest: string;
-}
-const editVisible = ref(false);
+const editVisible = ref(true);
 const curr: Ref<number> = ref(0);
-const rankTable: Ref<RankTableItem[]> = ref([]);
+const rankTable: Ref<any[]> = ref([]);
+const b0: Ref<Song> = ref({} as Song);
+const b19: Ref<Song[]> = ref(Array.from({ length: 20 }, () => ({} as Song)));
+const songDifficulties: Ref<any> = ref(null);
 
-onMounted(() => {
-    for (let i = 0; i < 20; i++) {
-        rankTable.value.push({ song: 'test', difficulty: '', rank: '0' } as RankTableItem);
-    }
+watch(() => b19,
+    () => {
+        // fix: 996
+        songDifficulties.value = Object.values(rankTable.value[b19.value[curr.value].song as any])
+            .filter((item: any) => item != '' && !isNaN(parseFloat(item)))
+            .map((item: any) => parseFloat(item))
+            .sort((a: any, b: any) => b - a);
+    },
+    { deep: true }
+)
+
+onMounted(async () => {
+    const response = await fetch('/api/v1/rank_table');
+    const data = await response.json();
+    rankTable.value = data;
 })
 </script>
