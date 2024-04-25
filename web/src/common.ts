c8da76f6f3c029363bb85708284e1be79e9d0846
xeonds <xeonds@stu.xidian.edu.cn>
2024-04-25 23:33:20 +0800

    feat: add history page

---
+++ web/src/common.ts
@@ -0,0 +1,55 @@
+export interface Song {
+  num: number;
+  song: string;
+  illustration: string;
+  rank: string;
+  difficulty: string;
+  rks: string;
+  Rating: string;
+  score: string;
+  acc: string;
+  suggest: string;
+}
+
+export const getRating = (fc: boolean, score: number) => {
+  if (score >= 1000000) return "phi";
+  if (fc) return "FC";
+  if (score >= 960000) return "V";
+  if (score >= 920000) return "S";
+  if (score >= 880000) return "A";
+  if (score >= 820000) return "B";
+  if (score >= 700000) return "C";
+  return "F";
+};
+
+export const parseSongList = (songList: any) => {
+  let result: Song[] = [] as Song[];
+  result = songList.map((item: any) => ({
+    num: 0,
+    song: item.Song || "",
+    illustration: item.Illustration || "",
+    rank: item.Level || "",
+    difficulty: item.Difficulty || "",
+    rks: item.Rks.toString() || "",
+    Rating: getRating(item.FullCombo, item.Score) || "",
+    score: item.Score.toString() || "",
+    acc: (item.Acc + 0.005).toFixed(2).toString() || "",
+    suggest: "",
+  }));
+  return result;
+};
+
+export const parseSong = (song: any) => {
+  return {
+    num: 0,
+    song: song.Song || "",
+    illustration: song.Illustration || "",
+    rank: song.Level || "",
+    difficulty: song.Difficulty || "",
+    rks: song.Rks.toString() || "",
+    Rating: getRating(song.FullCombo, song.Score) || "",
+    score: song.Score.toString() || "",
+    acc: (song.Acc + 0.005).toFixed(2).toString() || "",
+    suggest: "",
+  };
+};
--- web/src/views/history.vue
+++ web/src/views/history.vue
@@ -1,31 +1,64 @@
 <template>
-    <div class="p-4">
-        <h1 class="text-2xl font-bold mb-4 text-gray-400">查询历史</h1>
-        <div class="flex items-center mb-4">
-            <button @click="exportHistory" class="px-4 py-2 text-white btn btn-outline btn-primary">
-                导出历史
-            </button>
-        </div>
+    <h1 class="text-2xl font-bold mb-4 text-gray-400">查询历史</h1>
+    <div class="flex items-center mb-4">
+        <button @click="importHistory" class="mx-2 px-4 py-2 text-white btn btn-outline btn-primary">
+            导入历史
+        </button>
+        <button @click="exportHistory" class="mx-2 px-4 py-2 text-white btn btn-outline btn-primary">
+            导出历史
+        </button>
+        <button v-if="!isSelectHistory" @click="isSelectHistory = true"
+            class="mx-2 px-4 py-2 text-white btn btn-outline btn-primary">
+            选择历史
+        </button>
+    </div>
+    <div v-if="isSelectHistory">
+        <h3>查询历史记录，共{{ historyScores.length }}条</h3>
         <table class="table w-full border-collapse text-gray-400">
             <thead>
                 <tr>
-                    <th class="py-2 px-4 border-b text-gray-400">记录</th>
+                    <th class="py-2 px-4 border-b text-gray-400"></th>
+                    <th class="py-2 px-4 border-b text-gray-400">日期</th>
+                    <th class="py-2 px-4 border-b text-gray-400">Rks</th>
+                    <th class="py-2 px-4 border-b text-gray-400">操作</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="score in historyScores" :key="score">
-                    <td class="py-2 px-4 border-b text-gray-400">{{ score }}</td>
+                <tr v-for="(score, index) in historyScores" :key="score">
+                    <td class="py-2 px-4 border-b text-gray-400">
+                        <button class="px-2 py-1 btn btn-outline btn-info" @click="checkScore(score)">查 看</button>
+                    </td>
+                    <td class="py-2 px-4 border-b text-gray-400">{{ score.date }}</td>
+                    <td class="py-2 px-4 border-b text-gray-400">{{ score.rks }}</td>
+                    <td class="py-2 px-4 border-b">
+                        <button @click="deleteHistory(index)" class="px-2 py-1 btn btn-outline btn-warning">
+                            删除该记录
+                        </button>
+                    </td>
                 </tr>
             </tbody>
         </table>
+    </div>
+    <div v-else>
+        <p>Player: {{ selectedScore.player }}</p>
+        <p>RankingScore: {{ selectedScore.rks }}</p>
+        <div class="flex flex-row flex-wrap">
+            <song :index="'#phi'" :song="parseSong(selectedScore.phi)" class="mx-4"/>
+            <song v-for="(song, index) in parseSongList(selectedScore.b19)" :key="index"
+                :index="'#' + (index + 1).toString()" :song="song" class="mx-4"/>
+        </div>
     </div>
 </template>
 
 <script lang="ts" setup>
 import { Ref, onMounted, ref } from 'vue';
+import song from '@/components/song.vue';
+import { parseSongList, parseSong } from '@/common';
 
 const historyScores: Ref<any[]> = ref([])
 const sessionToken = ref<string>('');
+const isSelectHistory = ref(true);
+const selectedScore = ref<any>({});
 
 const exportHistory = () => {
     const data = JSON.stringify({ data: historyScores.value });
@@ -37,22 +70,49 @@ const exportHistory = () => {
     a.click();
     URL.revokeObjectURL(url);
 }
+const importHistory = () => {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.json';
+    input.onchange = (e) => {
+        const file = (e.target as HTMLInputElement).files?.[0];
+        if (!file) {
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const data = JSON.parse(e.target?.result as string);
+            historyScores.value = data.data;
+            localStorage.setItem('history-' + sessionToken.value, JSON.stringify(data));
+        }
+        reader.readAsText(file);
+    }
+    input.click();
+}
 const askForSessionToken = () => {
-  var token = sessionStorage.getItem('selectedSession');
-  if (!token) {
-    return false;
-  } else {
-    sessionToken.value = token!;
-    return true;
-  }
+    var token = sessionStorage.getItem('selectedSession');
+    if (!token) {
+        return false;
+    } else {
+        sessionToken.value = token!;
+        return true;
+    }
+}
+const deleteHistory = (index: number) => {
+    historyScores.value.splice(index, 1);
+    localStorage.setItem('history-' + sessionToken.value, JSON.stringify({ data: historyScores.value }));
+}
+const checkScore = (score: any) => {
+    selectedScore.value = score;
+    isSelectHistory.value = false;
 }
 
 onMounted(() => {
     if (!askForSessionToken()) {
-      alert('Please select a session first');
-      window.location.href = '/#/session';
+        alert('Please select a session first');
+        window.location.href = '/#/session';
     }
-    historyScores.value = JSON.parse(localStorage.getItem('history-'+sessionToken.value)||'[]').data;
+    historyScores.value = JSON.parse(localStorage.getItem('history-' + sessionToken.value) || '[]').data;
 })
 </script>
 
